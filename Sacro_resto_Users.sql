f88793e6e2658f4b3d03c0d0969bf7458fe7280c
Rocio Iglesias <118459931+accccastle@users.noreply.github.com>
2023-03-27 16:43:41 -0300

    Create users

---
+++ Sacro_resto_Users.sql
@@ -0,0 +1,18 @@
+USE sacro_resto;
+
+-- Creacion usuario
+CREATE USER rocio@localhost IDENTIFIED BY 'Rocio993_';
+-- Permiso solo de select
+GRANT SELECT ON sacro_resto.* TO rocio@localhost;
+-- Ver permisos usuario
+SHOW GRANTS FOR rocio@localhost;
+
+-- Creacion usuario 2
+CREATE USER castle@localhost IDENTIFIED BY 'Castle993_';
+-- Permiso solo de lectura, insert y update de la bd
+GRANT SELECT, INSERT, UPDATE ON sacro_resto.* TO castle@localhost;
+-- Ver permisos usuario
+SHOW GRANTS FOR castle@localhost;
+
+USE mysql;
+SELECT * FROM user;
